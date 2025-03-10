b7669f18e10624a4b52564eb01d3a27adecf24cd
Kratos5228 <81490639+Kratos5228@users.noreply.github.com>
2025-03-10 14:44:05 -0500

    Latest

---
+++ Desktop/desktop_app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import sqlite3
+
+root_window = Tk()
+root_window.geometry("3024x1964")
+root_window.configure(bg = '#DACEC4')
+root_window.title("To-Do List Application")
+
+
+
+
+root_window.mainloop()

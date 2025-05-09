555dc687bf3eef50fcc8978b03a89513ca483567
Rodolfo Gonzalez <rodolfojgs@hotmail.com>
2025-05-09 12:21:26 -0500

    Adding connection from signin to users in database

--- Desktop/functions/login.py
+++ Desktop/functions/login.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Label, Entry, Button, LEFT, RIGHT
+from tkinter import Checkbutton, Frame, Label, Entry, Button, LEFT, RIGHT, IntVar
 from . import misc, main_page, root_window
 import sqlite3
 
@@ -16,7 +16,6 @@ def check_login(username, password, frame):
 
     else:
         misc.pop_up("INVALID LOG IN")
-    return
 
 
 def login():
@@ -58,6 +57,7 @@ def login():
 def check_signup(username, password, frame):
     if len(password) < 4:
         misc.pop_up("Password must be more 4 or more characters")
+        return -1
     else:
         dataConnector = sqlite3.connect("toDo.db")
         cursor = dataConnector.cursor()
@@ -68,7 +68,26 @@ def check_signup(username, password, frame):
         dataConnector.commit()
         misc.clear_screen(frame)
         login()
+        return cursor.lastrowid
+
+
+def register_user(fname, lname, privilige, username, password, frame):
+    signup_id = check_signup(username, password, frame)
+    if signup_id == -1:
         return
+    else:
+        dataConnector = sqlite3.connect("toDo.db")
+        cursor = dataConnector.cursor()
+
+        cursor.execute(
+            "INSERT INTO Users (first_name,last_name,admin) VALUES (?,?,?)",
+            [fname, lname, privilige],
+        )
+        user_id = cursor.lastrowid
+        cursor.execute(
+            "INSERT INTO Login_to_User (sID,uID) VALUES (?,?)", [signup_id, user_id]
+        )
+        dataConnector.commit()
 
 
 def signup():
@@ -78,14 +97,35 @@ def signup():
     signInlabel = Label(signup_frame, text="Sign Up", bg="#DACEC4", font=("", 24))
     user_label = Label(user_frame, text="Username: ", bg="#DACEC4")
     user_entry = Entry(user_frame)
+    first_frame = Frame(signup_frame, bg="#DACEC4")
+    first_label = Label(first_frame, text="First Name: ", bg="#DACEC4")
+    first_entry = Entry(first_frame)
+    last_frame = Frame(signup_frame, bg="#DACEC4")
+    last_label = Label(last_frame, text="Last Name: ", bg="#DACEC4")
+    last_entry = Entry(last_frame)
     password_frame = Frame(signup_frame, bg="#DACEC4")
     password_label = Label(password_frame, text="Password: ", bg="#DACEC4")
     password_entry = Entry(password_frame, show="*")
+    check_frame = Frame(signup_frame, bg="#DACEC4")
+    check_var = IntVar(master=check_frame)
+    check_button = Checkbutton(
+        check_frame,
+        text="Admin?",
+        variable=check_var,
+        onvalue=1,
+        offvalue=0,
+        bg="#DACEC4",
+    )
     signup_button = Button(
         signup_frame,
         text="Sign Up",
-        command=lambda: check_signup(
-            user_entry.get(), password_entry.get(), signup_frame
+        command=lambda: register_user(
+            first_entry.get(),
+            last_entry.get(),
+            check_var.get(),
+            user_entry.get(),
+            password_entry.get(),
+            signup_frame,
         ),
     )
     to_login_button = Button(
@@ -98,10 +138,18 @@ def signup():
     signup_frame.pack(anchor="center", pady=10, expand=True)
     signInlabel.pack(pady=10)
     user_frame.pack()
+    first_frame.pack()
+    last_frame.pack()
     password_frame.pack()
+    check_frame.pack()
     user_label.pack(side=LEFT, padx=5, pady=5)
     user_entry.pack(side=RIGHT, padx=5, pady=5)
+    first_label.pack(side=LEFT, padx=5, pady=5)
+    first_entry.pack(side=RIGHT, padx=5, pady=5)
+    last_label.pack(side=LEFT, padx=5, pady=5)
+    last_entry.pack(side=RIGHT, padx=5, pady=5)
     password_label.pack(side=LEFT, padx=5, pady=5)
     password_entry.pack(side=RIGHT, padx=5, pady=5)
+    check_button.pack(anchor="center")
     signup_button.pack(anchor="center")
     to_login_button.pack(anchor="center")
